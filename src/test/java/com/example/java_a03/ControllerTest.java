cea7aa5ac69d515bb04130a5e22b176759c5eafb
markc <119313201@umail.ucc.ie>
2021-12-01 11:10:25 +0000

    Improved tests that were created yesterday, have 5 tests for both email and password in the ControllerTest class, will create the methods in order to see if the tests pass or fail

--- src/test/java/com/example/java_a03/ControllerTest.java
+++ src/test/java/com/example/java_a03/ControllerTest.java
@@ -1,42 +1,75 @@
 package com.example.java_a03;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 public class ControllerTest {
+    String userEmail = "Dave@Dave.com";
+    String userEmail2 = "";
+    String userEmail3 = "Dave@Dave";
+    String userEmail4 = "DaveDave.com";
+    String userEmail5 = "Dave@Dave.co.uk";
 
+    String userPassword = "Cool*s123456";
+    String userPassword2 = "Cools*";
+    String userPassword3 = "*123456";
+    String userPassword4 = "Cools123456";
+    String userPassword5 = "Cool*1";
 
     @Test
     public void testIfEmailIsNotNull() {
-        assertNotNull(userEmail);
+        assertFalse(isEmailNull(userEmail));
+        assertFalse(isEmailNull(userEmail2));
+        assertFalse(isEmailNull(userEmail3));
+        assertFalse(isEmailNull(userEmail4));
+        assertFalse(isEmailNull(userEmail5));
     }
 
     @Test
     public void testIfEmailIsWellFormed(){
-        assertTrue(userEmail.endsWith(".com"));
-        assertTrue(userEmail.contains("@"));
+        assertTrue(isEmailWellFormed(userEmail));
+        assertTrue(isEmailWellFormed(userEmail2));
+        assertTrue(isEmailWellFormed(userEmail3));
+        assertTrue(isEmailWellFormed(userEmail4));
+        assertTrue(isEmailWellFormed(userEmail5));
     }
 
     @Test
     public void testIfPasswordIsCorrectLength(){
-        assertTrue(userPassword.length()>= 7);
+        assertTrue(isLengthCorrect(userPassword));
+        assertTrue(isLengthCorrect(userPassword2));
+        assertTrue(isLengthCorrect(userPassword3));
+        assertTrue(isLengthCorrect(userPassword4));
+        assertTrue(isLengthCorrect(userPassword5));
     }
 
     @Test
     public void testIfPasswordHasNumber(){
-        assertTrue(// pass in test here);
+        assertTrue(isNumberInPassword(userPassword));
+        assertTrue(isNumberInPassword(userPassword2));
+        assertTrue(isNumberInPassword(userPassword3));
+        assertTrue(isNumberInPassword(userPassword4));
+        assertTrue(isNumberInPassword(userPassword5));
+
     }
 
 
     @Test
     public void testIfPasswordHasLetter(){
-        assertTrue(// pass in test here);
+        assertTrue(isLetterInPassword(userPassword));
+        assertTrue(isLetterInPassword(userPassword2));
+        assertTrue(isLetterInPassword(userPassword3));
+        assertTrue(isLetterInPassword(userPassword4));
+        assertTrue(isLetterInPassword(userPassword5));
     }
 
     @Test
     public void testIfPasswordHasSpecialCharacter(){
-        assertTrue(// pass in test here);
+        assertTrue(isSpecialCharacterInPassword(userPassword));
+        assertTrue(isSpecialCharacterInPassword(userPassword2));
+        assertTrue(isSpecialCharacterInPassword(userPassword3));
+        assertTrue(isSpecialCharacterInPassword(userPassword4));
+        assertTrue(isSpecialCharacterInPassword(userPassword5));
+
     }
 
 
