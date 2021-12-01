ac61f560d9230073aa739c5757de4854d151183b
markc <119313201@umail.ucc.ie>
2021-12-01 11:41:45 +0000

    Created the methods inside the HelloController class in order to test the email and password, once this was done I modified the tests, so they were able to run without errors. Once I ran the tests they all failed which was what I expected the tests to do, will investigate further.

--- src/main/java/com/example/java_a03/HelloController.java
+++ src/main/java/com/example/java_a03/HelloController.java
@@ -2,10 +2,42 @@ package com.example.java_a03;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import java.util.regex.*;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    public boolean isEmailNull(String email){
+        return email.isEmpty();
+    }
+
+    public boolean isEmailWellFormed(String email){
+        return email.endsWith(".com") && email.contains("@");
+    }
+
+    public boolean isLengthCorrect(String password){
+        return password.length() >= 7;
+    }
+
+    public boolean isNumberInPassword(String password){
+        Pattern p1 = Pattern.compile("[1234567890]");
+        Matcher m1 = p1.matcher(password);
+        return m1.find();
+    }
+
+    public boolean isLetterInPassword(String password){
+        Pattern p2 = Pattern.compile("[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]");
+        Matcher m2 = p2.matcher(password);
+        return m2.find();
+    }
+
+    public boolean isSpecialCharacterInPassword(String password){
+        Pattern p3 = Pattern.compile("[&^*!@]");
+        Matcher m3 = p3.matcher(password);
+        return m3.find();
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
--- src/test/java/com/example/java_a03/ControllerTest.java
+++ src/test/java/com/example/java_a03/ControllerTest.java
@@ -1,8 +1,11 @@
 package com.example.java_a03;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ControllerTest {
+    private HelloController controller;
     String userEmail = "Dave@Dave.com";
     String userEmail2 = "";
     String userEmail3 = "Dave@Dave";
@@ -10,65 +13,70 @@ public class ControllerTest {
     String userEmail5 = "Dave@Dave.co.uk";
 
     String userPassword = "Cool*s123456";
-    String userPassword2 = "Cools*";
+    String userPassword2 = "Coolss*";
     String userPassword3 = "*123456";
     String userPassword4 = "Cools123456";
     String userPassword5 = "Cool*1";
 
+    @BeforeEach
+    void init(){
+        controller = new HelloController();
+    }
+
     @Test
     public void testIfEmailIsNotNull() {
-        assertFalse(isEmailNull(userEmail));
-        assertFalse(isEmailNull(userEmail2));
-        assertFalse(isEmailNull(userEmail3));
-        assertFalse(isEmailNull(userEmail4));
-        assertFalse(isEmailNull(userEmail5));
+        assertFalse(controller.isEmailNull(userEmail));
+        assertFalse(controller.isEmailNull(userEmail2));
+        assertFalse(controller.isEmailNull(userEmail3));
+        assertFalse(controller.isEmailNull(userEmail4));
+        assertFalse(controller.isEmailNull(userEmail5));
     }
 
     @Test
     public void testIfEmailIsWellFormed(){
-        assertTrue(isEmailWellFormed(userEmail));
-        assertTrue(isEmailWellFormed(userEmail2));
-        assertTrue(isEmailWellFormed(userEmail3));
-        assertTrue(isEmailWellFormed(userEmail4));
-        assertTrue(isEmailWellFormed(userEmail5));
+        assertTrue(controller.isEmailWellFormed(userEmail));
+        assertTrue(controller.isEmailWellFormed(userEmail2));
+        assertTrue(controller.isEmailWellFormed(userEmail3));
+        assertTrue(controller.isEmailWellFormed(userEmail4));
+        assertTrue(controller.isEmailWellFormed(userEmail5));
     }
 
     @Test
     public void testIfPasswordIsCorrectLength(){
-        assertTrue(isLengthCorrect(userPassword));
-        assertTrue(isLengthCorrect(userPassword2));
-        assertTrue(isLengthCorrect(userPassword3));
-        assertTrue(isLengthCorrect(userPassword4));
-        assertTrue(isLengthCorrect(userPassword5));
+        assertTrue(controller.isLengthCorrect(userPassword));
+        assertTrue(controller.isLengthCorrect(userPassword2));
+        assertTrue(controller.isLengthCorrect(userPassword3));
+        assertTrue(controller.isLengthCorrect(userPassword4));
+        assertTrue(controller.isLengthCorrect(userPassword5));
     }
 
     @Test
     public void testIfPasswordHasNumber(){
-        assertTrue(isNumberInPassword(userPassword));
-        assertTrue(isNumberInPassword(userPassword2));
-        assertTrue(isNumberInPassword(userPassword3));
-        assertTrue(isNumberInPassword(userPassword4));
-        assertTrue(isNumberInPassword(userPassword5));
+        assertTrue(controller.isNumberInPassword(userPassword));
+        assertTrue(controller.isNumberInPassword(userPassword2));
+        assertTrue(controller.isNumberInPassword(userPassword3));
+        assertTrue(controller.isNumberInPassword(userPassword4));
+        assertTrue(controller.isNumberInPassword(userPassword5));
 
     }
 
 
     @Test
     public void testIfPasswordHasLetter(){
-        assertTrue(isLetterInPassword(userPassword));
-        assertTrue(isLetterInPassword(userPassword2));
-        assertTrue(isLetterInPassword(userPassword3));
-        assertTrue(isLetterInPassword(userPassword4));
-        assertTrue(isLetterInPassword(userPassword5));
+        assertTrue(controller.isLetterInPassword(userPassword));
+        assertTrue(controller.isLetterInPassword(userPassword2));
+        assertTrue(controller.isLetterInPassword(userPassword3));
+        assertTrue(controller.isLetterInPassword(userPassword4));
+        assertTrue(controller.isLetterInPassword(userPassword5));
     }
 
     @Test
     public void testIfPasswordHasSpecialCharacter(){
-        assertTrue(isSpecialCharacterInPassword(userPassword));
-        assertTrue(isSpecialCharacterInPassword(userPassword2));
-        assertTrue(isSpecialCharacterInPassword(userPassword3));
-        assertTrue(isSpecialCharacterInPassword(userPassword4));
-        assertTrue(isSpecialCharacterInPassword(userPassword5));
+        assertTrue(controller.isSpecialCharacterInPassword(userPassword));
+        assertTrue(controller.isSpecialCharacterInPassword(userPassword2));
+        assertTrue(controller.isSpecialCharacterInPassword(userPassword3));
+        assertTrue(controller.isSpecialCharacterInPassword(userPassword4));
+        assertTrue(controller.isSpecialCharacterInPassword(userPassword5));
 
     }
 
